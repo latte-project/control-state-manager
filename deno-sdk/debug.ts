c4f7e23bbf2109a631823b26632e6f9d0225f935
Milky2018 <37078809+Milky2018@users.noreply.github.com>
2022-04-08 20:32:06 +0800

    Fix bugs.

--- deno-sdk/debug.ts
+++ deno-sdk/debug.ts
@@ -1,11 +1,11 @@
 export function debug() {
     return function(target: any, name: string, descriptor: PropertyDescriptor) {
         const f = descriptor.value!;
-        descriptor.value = async function(...args: any[]) {
+        descriptor.value = function(...args: any[]) {
             console.info(`Function ${name} arguments: `);
             console.info(...args);
             try {
-                const res = await f.apply(this, args);
+                const res = f.apply(this, args);
                 console.info(`Function ${name} returns: `);
                 console.info(res);
                 return res;
